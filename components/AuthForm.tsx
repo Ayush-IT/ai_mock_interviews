7199746e5a7888d98bba967f708884d7269df1ab
Ayush <ayushbhardwaj3746@gmail.com>
2025-04-06 03:09:43 +0530

    implement auth and storage

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -13,6 +13,9 @@ import { Form } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 
 import FormField from "./FormField";
+import { signUp, signIn } from "@/lib/actions/auth.action";
+import { auth } from "@/firebase/client";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -35,53 +38,53 @@ const AuthForm = ({ type }: { type: FormType }) => {
     },
   });
 
-  // const onSubmit = async (data: z.infer<typeof formSchema>) => {
-    function onSubmit (values: z.infer<typeof formSchema>) {
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    
     try {
       if (type === "sign-up") {
-        console.log("sign up", values);
-        // const { name, email, password } = data;
+        
+        const { name, email, password } = data;
 
-        // const userCredential = await createUserWithEmailAndPassword(
-        //   auth,
-        //   email,
-        //   password
-        // );
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
 
-        // const result = await signUp({
-        //   uid: userCredential.user.uid,
-        //   name: name!,
-        //   email,
-        //   password,
-        // });
+        const result = await signUp({
+          uid: userCredential.user.uid,
+          name: name!,
+          email,
+          password,
+        });
 
-        // if (!result.success) {
-        //   toast.error(result.message);
-        //   return;
-        // }
+        if (!result.success) {
+          toast.error(result.message);
+          return;
+        }  
 
         toast.success("Account created successfully. Please sign in.");
         router.push("/sign-in");
       } else {
-        console.log("sign in", values);
-        // const { email, password } = data;
+        
+        const { email, password } = data;
 
-        // const userCredential = await signInWithEmailAndPassword(
-        //   auth,
-        //   email,
-        //   password
-        // );
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
 
-        // const idToken = await userCredential.user.getIdToken();
-        // if (!idToken) {
-        //   toast.error("Sign in Failed. Please try again.");
-        //   return;
-        // }
+        const idToken = await userCredential.user.getIdToken();
+        if (!idToken) {
+          toast.error("Sign in Failed. Please try again.");
+          return;
+        }
 
-        // await signIn({
-        //   email,
-        //   idToken,
-        // });
+        await signIn({
+          email,
+          idToken,
+        });
 
         toast.success("Signed in successfully.");
         router.push("/");
